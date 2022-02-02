c6b4fec80cfb05fa77cf729f389fcab8fbbfd346
alsignat <alsignat@gmail.com>
2022-02-02 23:44:13 +0300

    7. ListIterator [#350217 #259482]

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -9,19 +9,13 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.nextIndex() != index) {
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index);
         iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.nextIndex() != index + 1) {
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
         iterator.add(value);
     }
 
