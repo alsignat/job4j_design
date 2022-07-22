d5b3b40d7ee13663a1eb935ec3cae87e2c599ca2
alsignat <alsignat@gmail.com>
2022-07-23 01:09:09 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analyzer.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analyzer {
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(target)))) {
+            boolean unavailableNow = false;
+            String ln;
+            while ((ln = in.readLine()) != null) {
+                String[] data = ln.split(" ");
+                int status = Integer.parseInt(data[0]);
+                String time = data[1];
+                if ((status == 400 || status == 500) && !unavailableNow) {
+                    out.print(time + " ");
+                    unavailableNow = true;
+                } else if (status != 400 && status != 500 && unavailableNow) {
+                    out.println(time);
+                    unavailableNow = false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analyzer analyzer = new Analyzer();
+        analyzer.unavailable("./data/server_time_in.txt", "./data/target.txt");
+    }
+
+}
